0bd3ac89c4c743b4cccf7a00ba7784c9f89dcae2
sitohari <sitohari01@gmail.com>
2025-08-12 14:20:57 +0700

    Respone Status

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const requestListener = (request, response) => {
  
     if(url === '/') {
         if(method === 'GET') {
+            response.statusCode = 200; // OK
             response.end('<h1>Ini adalah homepage</h1>');
         } else {
+            response.statusCode = 400; // Method Not Allowed
             response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
         }
     } else if(url === '/about') {
         if(method === 'GET') {
+            response.statusCode = 200; // OK
             response.end('<h1>Halo! Ini adalah halaman about</h1>')
         } else if(method === 'POST') {
             let body = [];
@@ -25,13 +28,23 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+                response.statusCode = 200; // OK
                 response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
             });
         } else {
+            response.statusCode = 400; // Method Not Allowed
             response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
         }
     } else {
+        response.statusCode = 404; // Not Found
         response.end('<h1>Halaman tidak ditemukan!</h1>');
+    }
+
+    if(method === "PUT"){
+        // 
+    }
+    if(method === "DELETE"){
+        // 
     }
 };
  
